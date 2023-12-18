6eb2175855e6423b9b3ff8ac59317b3fd972b7b3
Александр Лукин <tallman2002@mail.ru>
2023-12-19 03:17:30 +0700

    Добавьте файлы проекта.

---
+++ WebApplication1/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1
+{
+    public class ChatApp : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Receive", message, userName);
+        }
+    }
+}
+
